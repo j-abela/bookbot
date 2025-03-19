213a3c04910915c53df9138e15f1dfca4ff683b3
j-abela <jacobabela@gmail.com>
2025-03-20 02:16:40 +0800

    implemented sorting function and report generator

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from stats import *
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = word_count(book_text)
-    num_chars = count_chars(book_text)
-    print(f"{num_words} words found in the document") 
-    print(num_chars)                  
+    file_path = "books/frankenstein.txt"
+    book_text = get_book_text(file_path)
+    num_words = get_word_count(book_text)
+    num_chars = get_char_count(book_text)
+    sorted_char_count = sort_char_count(num_chars)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for line in sorted_char_count:
+        if line["character"].isalpha():
+            print(f"{line['character']}: {line['count']}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,15 @@
-def word_count(book_text):
-    words = book_text.split()
-    num_words = len(words)
-    return num_words
-
 def get_book_text(file_path):
     book_text = ""
     with open(file_path) as f:
         book_text += f.read()
     return book_text
 
-def count_chars(book_text):
+def get_word_count(book_text):
+    words = book_text.split()
+    num_words = len(words)
+    return num_words
+
+def get_char_count(book_text):
     char_list = []
     char_count = {}
     for char in book_text:
@@ -21,5 +21,17 @@ def count_chars(book_text):
             char_count[char] = 1
         else:
             char_count[char] +=1
-    
     return char_count
+
+def sort_char_count(big_dict):
+    sorted_list = []
+    for key in big_dict:
+        char_dict = {}
+        char_dict["character"] = key
+        char_dict["count"] = big_dict[key]
+        sorted_list.append(char_dict)
+    sorted_list.sort(reverse = True, key=sort_on)
+    return sorted_list
+
+def sort_on(dict):
+    return dict["count"]
