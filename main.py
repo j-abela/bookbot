ddcaf4e03911a18c44df2f4143e8803be8623193
j-abela <jacobabela@gmail.com>
2025-03-20 15:05:57 +0800

    Implemented using sys.argv instead of hard-coding path to book

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import sys
 from stats import *
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
     book_text = get_book_text(file_path)
-    num_words = get_word_count(book_text)
+    word_count = get_word_count(book_text)
     num_chars = get_char_count(book_text)
     sorted_char_count = sort_char_count(num_chars)
 
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {file_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
+    print(f"Found {word_count} total words")
     print("--------- Character Count -------")
     for line in sorted_char_count:
         if line["character"].isalpha():
